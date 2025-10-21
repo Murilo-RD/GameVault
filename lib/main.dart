bb6a4bc4e749ca7a9daf30fe45ebb9869169b334
Murilo-RD <murilo.dhu@gmail.com>
2025-10-21 20:10:10 -0300

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MaterialApp(home: HomePage()));
 }
 
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF1E1E1E),
+      appBar: AppBar(
+        toolbarHeight: 130,
+        backgroundColor: Color(0xFF1E1E1E),
+        title: Align(
+          alignment: Alignment.bottomLeft,
+          child: Text(
+            "Olá, Murilo!",
+            style: TextStyle(
+              color: Color(0xFFE0E0E0),
+              fontSize: 40,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+      ),
+      body: body(),
+    );
+  }
+}
+
+ Widget body(){
+  return Column(
+    children: [
+      Container(
+        child: Column(
+
+        ),
+      )
+    ],
+
+  );
+ }
