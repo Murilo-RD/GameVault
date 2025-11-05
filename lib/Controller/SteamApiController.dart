dab35d5d037b63c0eb7d2a291d1da205295c829f
Murilo <murilo.dhu@gmail.com>
2025-11-05 19:34:17 -0300

    configuração Api

--- lib/Controller/SteamApiController.dart
+++ lib/Controller/SteamApiController.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:game_vault/Service/ApiService.dart';
 
 class SteamApiController {
@@ -5,10 +7,35 @@ class SteamApiController {
    final _userStats = ApiService(baseUrl: 'https://api.steampowered.com/ISteamUserStats/');
    final _user = ApiService(baseUrl: 'https://api.steampowered.com/ISteamUser/');
    final _listaDesejo = ApiService(baseUrl: 'https://api.steampowered.com/IWishlistService/');
+   late final _usuario;
+   late final _KEY = dotenv.env['STEAM_API_KEY'];
 
+
+   set usuario(User usuario) {
+     _usuario = usuario;
+   }
 
    Future<dynamic> getUserData() async {
-     return _user.get('GetPlayerSummaries/v0002/?key=<YOUR_KEY>&steamids=<STEAMID>');
+     return _user.get('GetPlayerSummaries/v0002/?key=$_KEY&steamids=${_usuario.uid}');
    }
+
+   Future<dynamic> getRecenteJogados() async {
+     return _playerService.get('GetRecentlyPlayedGames/v0001/?key=$_KEY&steamids=${_usuario.uid}&format=json');
+   }
+
+   Future<dynamic> getUserGames() async {
+     return _playerService.get('GetOwnedGames/v0001/?$_KEY&steamids=${_usuario.uid}&include_appinfo=1&include_played_free_games=1&format=json');
+   }
+
+   Future<dynamic> getGameAchievements(String appId) async {
+     return _playerService.get('GetPlayerAchievements/v0001/?appid=$appId&$_KEY&steamids=${_usuario.uid}');
+   }
+
+   Future<dynamic> getGameStats(String appId) async {
+     return _playerService.get('GetUserStatsForGame/v0002/?appid=$appId&$_KEY&steamids=${_usuario.uid}');
+   }
+
+
+
 
 }
